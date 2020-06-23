c762b7807fbb3f86a8cd1d1e4d2a364e4ad7e76e
SeregaLucky <strizgsergey@gmail.com>
2020-06-23 14:00:12 +0300

    add

--- module_14/part_II/fileReader.js
+++ module_14/part_II/fileReader.js
@@ -1,17 +1,17 @@
-const input = document.querySelector('.js_input_file');
-const img = document.querySelector('.js_img');
+const input = document.querySelector('.js_input_file')
+const img = document.querySelector('.js_img')
 
-input.addEventListener('change', handleChange);
+input.addEventListener('change', handleChange)
 
 function handleChange(e) {
-  const file = e.target.files[0];
-  const reader = new FileReader();
+  const file = e.target.files[0]
+  const reader = new FileReader()
   // console.log(reader);
 
   reader.onloadend = () => {
-    console.log(reader);
-    img.src = reader.result;
-  };
+    console.log(reader)
+    img.src = reader.result
+  }
 
-  reader.readAsDataURL(file);
+  reader.readAsDataURL(file)
 }
--- module_14/part_II/images.js
+++ module_14/part_II/images.js
@@ -1,43 +1,43 @@
-const list = document.querySelector('.list_emply');
-let elTake = null;
+const list = document.querySelector('.list_emply')
+let elTake = null
 
-list.addEventListener('dragstart', dragStart);
-list.addEventListener('dragend', dragEnd);
+list.addEventListener('dragstart', dragStart)
+list.addEventListener('dragend', dragEnd)
 
-list.addEventListener('dragenter', dragEnter);
-list.addEventListener('dragleave', dragLeave);
+list.addEventListener('dragenter', dragEnter)
+list.addEventListener('dragleave', dragLeave)
 
-list.addEventListener('dragover', dragOver);
-list.addEventListener('drop', dragDrop);
+list.addEventListener('dragover', dragOver)
+list.addEventListener('drop', dragDrop)
 
-function dragStart({target}) {
-  if (target.nodeName !== 'DIV') return;
-  console.log('dragStart');
-  target.classList.add('hold');
-  elTake = target;
+function dragStart({ target }) {
+  if (target.nodeName !== 'DIV') return
+  console.log('dragStart')
+  target.classList.add('hold')
+  elTake = target
 }
-function dragEnd({target}) {
-  if (target.nodeName !== 'DIV') return;
-  console.log('dragEnd');
-  target.classList.remove('hold');
+function dragEnd({ target }) {
+  if (target.nodeName !== 'DIV') return
+  console.log('dragEnd')
+  target.classList.remove('hold')
 }
 
-function dragEnter({target}) {
-  if (target.nodeName !== 'LI') return;
-  target.classList.add('hovered');
+function dragEnter({ target }) {
+  if (target.nodeName !== 'LI') return
+  target.classList.add('hovered')
 }
-function dragLeave({target}) {
-  if (target.nodeName !== 'LI') return;
-  target.classList.remove('hovered');
+function dragLeave({ target }) {
+  if (target.nodeName !== 'LI') return
+  target.classList.remove('hovered')
 }
 
 function dragOver(e) {
-  e.preventDefault();
-  console.log(111);
+  e.preventDefault()
+  console.log(111)
 }
-function dragDrop({target}) {
-  if (target.nodeName !== 'LI') return;
-  target.classList.remove('hovered');
-  target.append(elTake);
-  elTake = null;
+function dragDrop({ target }) {
+  if (target.nodeName !== 'LI') return
+  target.classList.remove('hovered')
+  target.append(elTake)
+  elTake = null
 }
